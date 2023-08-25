bc4937bd0d3275ffee6c932a6f3ce9c0453c9970
Stanislav Kintoriak <kintoriak@gmail.com>
2023-08-26 00:06:49 +0300

    Commit 1
    1) Changed endpoint at app.js for swagger page ( /api/docs to /api/v1/docs);
    2) Added some code to swagger.js;
    Now link to swagger docs page works, two endpoints (/users/register and users/login) are described and try it out (sending requests for swagger) for them works as well.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const swaggerUi = require("swagger-ui-express");
 
 const swaggerDocument = require("./swagger.json");
 
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // ================================================= ERROR CATCHERS =========================================
 
