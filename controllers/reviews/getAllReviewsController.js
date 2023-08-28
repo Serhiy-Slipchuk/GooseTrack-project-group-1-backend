47e9c8261c2a7542c259acb30f87f7acf66ab7a2
Serhiy Slipchuk <114355951+Serhiy-Slipchuk@users.noreply.github.com>
2023-08-28 05:17:34 +0300

    Update getAllReviewsController.js

--- controllers/reviews/getAllReviewsController.js
+++ controllers/reviews/getAllReviewsController.js
@@ -3,7 +3,7 @@ const { User } = require("../../models/user")
 
 const getAllReviewsController = async (req, res) => {
     try {
-        const reviewsList = await Review.find().populate({ path: 'owner', select: 'name role -_id' });
+        const reviewsList = await Review.find().populate({ path: 'owner', select: 'name role avatarURL -_id' });
         res.status(200).json({status: 200, message: "Success", reviews: reviewsList});
     } catch (error) {
         console.log(error.message);
