88477f19bd24c89b748e201b2f6e879f306c93d2
IrynaKyslytsia <114502274+IrynaKyslytsia@users.noreply.github.com>
2023-08-28 16:42:09 +0300

    add time validation

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,6 +3,14 @@ const AppError = require("../services/appError");
 const validateBody = schema => {
     const func = (req, res, next) => {
         const {error} = schema.validate(req.body);
+        const { start, end } = req.body;
+
+        if(start >= end) {
+          res.status(400).json({
+            status: 400, message: "Start time must be less than end time"
+          });
+          return;
+        };
 
         if(error) {
         next(new AppError(400, error.message))
