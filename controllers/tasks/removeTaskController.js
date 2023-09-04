01a0c617d67f7a8106ffedaf2766e22cc954e7f3
Serhiy Slipchuk <slipchuks@ukr.net>
2023-09-05 02:55:57 +0300

    update

--- controllers/tasks/removeTaskController.js
+++ controllers/tasks/removeTaskController.js
@@ -1,20 +1,26 @@
 const { Task } = require("../../models/task");
 
 const removeTaskController = async (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    try {
-        const result = await Task.findByIdAndRemove(id);
+  try {
+    const currentTask = await Task.findById(id);
 
-    if(!result) {
-        res.status(404).json({ status: 404, message: "Not found" });
-        return;
-    };
-
-    res.status(200).json({ status: 200, message: "Task deleted"});
-    } catch (error) {
-        res.status(500).json({ status: 500, message: error.message });
+    if (!currentTask) {
+      res.status(404).json({ status: 404, message: "Not found" });
+      return;
     }
+
+    if (currentTask.owner.toString() !== req.user._id.toString()) {
+      res.status(423).json({ status: 423, message: "Locked. You have no necessary permissions to remove this task" });
+      return;
+    }
+    await Task.findByIdAndDelete(id);
+
+    res.status(200).json({ status: 200, message: "Task deleted" });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error.message });
+  }
 };
 
 module.exports = { removeTaskController };
